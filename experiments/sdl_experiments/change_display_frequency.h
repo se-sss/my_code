1ae0148ca06b2e578d3ee2126a38ce5c15665a84
se-sss <se-sss@yandex.ru>
2010-11-17 13:31:57 +0300

    change_display_frequency.c

---
+++ experiments/sdl_experiments/change_display_frequency.h
@@ -0,0 +1,15 @@
+#ifndef CHANGE_DISPLAY_FREQUENCY_H_INCLUDED
+#define CHANGE_DISPLAY_FREQUENCY_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  unsigned change_display_frequency(unsigned freq);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
