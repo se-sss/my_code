ceacfd4b38acdb6b9f309512c586f6b7f328b6c6
se-sss <se-sss@yandex.ru>
2015-09-01 21:58:06 +0300

    try to make sdl program in linux

--- experiments/sdl_experiments/placeholder.cpp
+++ experiments/sdl_experiments/placeholder.cpp
@@ -1,6 +1,19 @@
 #include <cstdio>
+#include "SDL.h"
 
 int main()
 {
    printf("Hello world!\n");
+   if(SDL_Init(SDL_INIT_VIDEO) < 0)
+   {
+       fprintf(stderr, "Cannot init SDL: %s\n", SDL_GetError());
+   }
+
+   const SDL_VideoInfo* info = SDL_GetVideoInfo();
+   SDL_Rect** modes = SDL_ListModes(NULL, SDL_FULLSCREEN);
+   printf("Available modes:\n");
+   for(int i = 0; modes[i]; ++i)
+   {
+       printf("   %d x %d\n", modes[i]->w, modes[i]->h);
+   }
 }
