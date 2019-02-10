149f5b0ac8f764e4fce3f4a58d7979c50a013316
se-sss <se-sss@yandex.ru>
2019-02-10 10:24:56 +0300

    clean

--- qw/reasonable.c
+++ qw/reasonable.c
@@ -3,22 +3,21 @@
 
 int main()
 {
-  const float f = 6.f;
+  const float f = 4.f;
   const float a = 10.f;
   const float T = 0.013f;
   float v_max = 320.f;
+  float v_ground_reasonable = 0.f;
+  float v_ground_ultimate = 0.f;
 
-  float v_ground_reasonable =
-    (float) sqrt((-30 * 30 + a * T * (2 - a * T) * v_max * v_max) /
-                 (f * T * (2 - f * T)));
-
-  while (1)
-  {
-    scanf("%f", &v_max);
-    v_ground_reasonable =
+  scanf("%f", &v_max);
+  v_ground_reasonable =
       (float) sqrt((-30 * 30 + a * T * (2 - a * T) * v_max * v_max) /
                    (f * T * (2 - f * T)));
-    printf("v_max=%f  v_reasonable=%f\n", v_max, v_ground_reasonable);
-  }
+
+  v_ground_ultimate = v_max * (float) sqrt((a / f) * ((2 - a * T) / (2 - f * T)));
+
+  printf("v_max=%f  v_reasonable=%f v_ultimate=%f\n", v_max, v_ground_reasonable, v_ground_ultimate);
+
   return 0;
 }
